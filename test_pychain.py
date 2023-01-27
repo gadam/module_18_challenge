93b5f237b59cc4cc9f211d651887ffa4da0327d1
Guy Adam <guy.adam@gmail.com>
2023-01-27 20:24:44 +1100

    Refactored unit test to check that a valid block can be created

--- test_pychain.py
+++ test_pychain.py
@@ -3,18 +3,33 @@
 # Imports
 import unittest
 import pychain
+import re
 
 class TestPychain(unittest.TestCase):
     def setUp(self):
-        self.genesis = pychain.Block("Genesis", 0)
+        """Test record with values required by each test case"""
+        self.record = pychain.Record(
+            "Unit Test",
+            "Unit Tester",
+            999
+        )
+
 
     def test_block_can_be_created(self):
-        # Uses the setUp block as data to validate
-        self.assertEqual(self.genesis.record, "Genesis")
-        self.assertEqual(self.genesis.creator_id, 0)
-        self.assertEqual(self.genesis.prev_hash, "0")
-        self.assertEqual(self.genesis.nonce, 0)
-        self.assertIsNotNone(self.genesis.timestamp)
+        """Check that a valid block can be created"""
+        self.test_block = pychain.Block(
+            self.record,
+            0,
+            "0"
+        )
+        self.assertEqual(self.test_block.record.sender, "Unit Test")
+        self.assertEqual(self.test_block.record.receiver, "Unit Tester")
+        self.assertEqual(self.test_block.record.amount, 999)
+        self.assertEqual(self.test_block.creator_id, 0)
+        self.assertEqual(self.test_block.prev_hash, "0")
+        self.assertEqual(self.test_block.nonce, 0)
+        self.assertIsNotNone(self.test_block.timestamp)
+
 
 if __name__ == "__main__":
     unittest.main()
