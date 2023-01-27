68123dd2b3d28f45037a14df4ae6dedc3d661be4
Guy Adam <guy.adam@gmail.com>
2023-01-27 20:27:25 +1100

    Refactored unit test to make block creation part of setUp()

--- test_pychain.py
+++ test_pychain.py
@@ -13,15 +13,16 @@ class TestPychain(unittest.TestCase):
             "Unit Tester",
             999
         )
-
-
-    def test_block_can_be_created(self):
-        """Check that a valid block can be created"""
+        
         self.test_block = pychain.Block(
             self.record,
             0,
             "0"
         )
+
+    def test_block_can_be_created(self):
+        """Check that a valid block can be created"""
+
         self.assertEqual(self.test_block.record.sender, "Unit Test")
         self.assertEqual(self.test_block.record.receiver, "Unit Tester")
         self.assertEqual(self.test_block.record.amount, 999)
@@ -29,6 +30,7 @@ class TestPychain(unittest.TestCase):
         self.assertEqual(self.test_block.prev_hash, "0")
         self.assertEqual(self.test_block.nonce, 0)
         self.assertIsNotNone(self.test_block.timestamp)
+
 
 
 if __name__ == "__main__":
