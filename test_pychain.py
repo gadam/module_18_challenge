3c05422b00c2466524b562441ac9ff08f00f1a4b
Guy Adam <guy.adam@gmail.com>
2023-01-27 20:37:16 +1100

    Unit tested block hashing method

--- test_pychain.py
+++ test_pychain.py
@@ -13,7 +13,7 @@ class TestPychain(unittest.TestCase):
             "Unit Tester",
             999
         )
-        
+
         self.test_block = pychain.Block(
             self.record,
             0,
@@ -22,7 +22,6 @@ class TestPychain(unittest.TestCase):
 
     def test_block_can_be_created(self):
         """Check that a valid block can be created"""
-
         self.assertEqual(self.test_block.record.sender, "Unit Test")
         self.assertEqual(self.test_block.record.receiver, "Unit Tester")
         self.assertEqual(self.test_block.record.amount, 999)
@@ -31,7 +30,10 @@ class TestPychain(unittest.TestCase):
         self.assertEqual(self.test_block.nonce, 0)
         self.assertIsNotNone(self.test_block.timestamp)
 
-
+    def test_block_can_be_hashed(self):
+        """Check that block is correctly hashed using SHA256"""
+        sha_pattern = "^[a-fA-F0-9]{64}$"
+        self.assertTrue(re.match(sha_pattern, self.test_block.hash_block()))
 
 if __name__ == "__main__":
     unittest.main()
